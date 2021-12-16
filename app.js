47aa7f44727784bb0a8c91b6971804b721eb6397
iamrashidpathan <khanrshid11a@gmail.com>
2021-12-16 13:18:33 +0530

    Complete v2

--- app.js
+++ app.js
@@ -7,31 +7,42 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 iteams = ["Buy Food", "Cook Food", "Eat Food"];
+workItems= [];
 
 app.get("/", function(request, response){
     //response.send("Hello");
-    let today = new Date();
-
-    let options = { weekday: 'long', day: 'numeric', month: 'long'};
-
-
     //console.log(today.toLocaleDateString("en-US")); // 9/17/2016
     //console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
 
+    let today = new Date();
+
+    let options = { weekday: 'long', day: 'numeric', month: 'long'};
+    
     let day = today.toLocaleDateString("en-US", options);
 
-    //console.log(day)
-
-    response.render('list', {kindOfDay : day, newIteams : iteams});
+    response.render('list', {listTitle : day, newIteams : iteams});
 
 });
 
 app.post("/", function(request, response){
+
     let iteam = request.body.newIteam;
-    iteams.push(iteam);
+    console.log(request.body);
+
+    if(request.body.list === "Work"){
+        workItems.push(iteam);
+        response.redirect("/work");
+    }else{
+        iteams.push(iteam);
+        response.redirect("/");
+    }
     //console.log(iteam);
     //console.log("We are in post")
-    response.redirect("/");
+    
+});
+
+app.get("/work", function(request, response){
+    response.render('list', {listTitle: "Work List", newIteams: workItems});
 });
 
 app.listen(3000, function(){
